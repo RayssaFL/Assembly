d54a116e646d82cfe7a258f564305d6ba3732a90
RayssaFL <lrayssa232@gmail.com>
2025-04-13 18:10:18 -0300

    Update 1_programa_p1.asm

--- 1_programa_p1.asm
+++ 1_programa_p1.asm
@@ -1,5 +1,5 @@
 .data #area de declaracao de variaveis
-    letra: .byte 'F' #declarando a variavel "letra" como byte pois seu dado é apenas uma LETRA
+    letra: .byte 'F' #declarando a variavel "letra" como byte pois seu dado Ã© apenas uma LETRA
 .text #area para escrever o programa
     li $v0, 4
     la $a0, letra #sempre que for imprimir tem que usar isso
@@ -8,4 +8,3 @@
     li $v0, 10
     syscall
     
-##para string e char, mesma coisa, exceto pela declaracao da variavel
